6469f0bc0ff0a6a97398a1097616e0d6f91943e6
lix <lx626159@163.com>
2025-11-05 09:57:13 +0800

    项目初始化，添加@符适配，VueRouter适配

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+﻿import {RouterMap} from "./router.config"
+import {createRouter, createWebHistory} from "vue-router";
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    // 解决路由置于顶部
+    scrollBehavior: () => ({top: 0}),
+    routes: RouterMap,
+})
+
+//防止多次访问同一路由报错
+const originalPush = router.push
+router.push = function push(location) {
+    return originalPush.call(this, location).catch((err: any) => err)
+}
+
+// router.beforeEach((to, form, next) => {
+//
+// })
+
+
+export default router
---
+++ src/router/router.config.ts
@@ -0,0 +1,19 @@
+﻿export const RouterMap = [{
+    path: "/",
+    component: () => import(),
+    redirect: '/login',
+    children: [
+        {
+            path: "/home",
+            name: "Home",
+            component: () => import(),
+            meta: {
+                tabBar: true,
+                search: true,
+                navBar: false,
+                Auth: false
+            }
+
+        },
+    ]
+}]

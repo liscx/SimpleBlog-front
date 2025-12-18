ad114f497dbb6dade569e54d45a297f40a36a3c7
lix <lx626159@163.com>
2025-12-18 20:14:55 +0800

    增加工具栏 新增黑夜、白天主题切换功能

---
+++ src/components/ToolBar/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="left-tool-bar">
+    <div class="bar-divide"></div>
+    <div class="bar-divide"></div>
+    <div class="bar-divide"></div>
+    <div class="bar-divide"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+#left-tool-bar {
+  margin: 10px 0 10px 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  column-gap: 5px;
+
+  width: 100%;
+  height: 20px;
+
+  .bar-divide {
+    width: 95%;
+    height: 95%;
+    background-color: var(--custom-button-color);
+    transition: background-color 0.3s ease;
+  }
+  
+  .bar-divide:hover {
+    background-color: var(--custom-button-hover-color);
+  }
+}
+
+</style>

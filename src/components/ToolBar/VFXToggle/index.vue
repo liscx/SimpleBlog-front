6b8969608241a24e2c0fe5de1aef9f476a39805e
lix <lx626159@163.com>
2026-01-07 17:33:29 +0800

    新增背景特效开关

---
+++ src/components/ToolBar/VFXToggle/index.vue
@@ -0,0 +1,36 @@
+﻿<template>
+  <div id="VFXSwitchBtn">
+    <div v-if="isShow">
+      <!--关闭特效-->
+      <div @click="handleClick(false)" class="demo"></div>
+    </div>
+    <div v-else>
+      <!--打开特效-->
+      <div @click="handleClick(true)" class="demo"></div>
+    </div>
+
+  </div>
+</template>
+<script setup lang="ts">
+import {ref} from "vue";
+import {getCurrentInstance} from 'vue';
+
+const {emitter} = getCurrentInstance()!.appContext.config.globalProperties;
+const isShow = ref(true)
+
+
+const handleClick = (status: boolean) => {
+  emitter.emit('isShow', status);
+  isShow.value = status
+}
+
+</script>
+
+
+<style scoped>
+.demo {
+  width: 20px;
+  height: 20px;
+  background-color: pink;
+}
+</style>
